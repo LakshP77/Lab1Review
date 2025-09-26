9cd6877dabeebb28d84b7b6909b6069f68824ccf
LakshP77 <lspvsp@gmail.com>
2025-09-26 19:54:33 +0000

    task 2 done

--- Grader.java
+++ Grader.java
@@ -5,20 +5,46 @@
 // The loop should run as long as numGrades is not positive. 
 // In the body of the loop, remind the user to enter a positive number, and then read the user's input into numGrades again.
 
+
+
+// Ask the user to enter a series of grades, and store them in an array.
+// In the main method, declare an integer array just large enough to store the number of grades 
+// which the user wishes to process (numGrades).
+// Prompt the user to enter the grades separated by spaces (e.g., 94 75 82).
+// In a for loop, read the grades entered by the user and store them in the array.
+
 import java.util.Scanner;
 
 public class Grader {
-   Scanner input = new Scanner(System.in);
-   
-    System.out.println("Enter the # of grades you want: ");
-    int numGrades = input.nextInt();
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-    while (numGrades <= 0) {
-        System.out.println("Please enter a postive # of grades: ");
-        numGrades = input.nextInt();
+        // Ask the user how many grades they want to process
+        System.out.print("Enter the number of grades you would like to process: ");
+        int numGrades = input.nextInt();
+
+        // Validation loop: keep asking until user enters a positive number
+        while (numGrades <= 0) {
+            System.out.print("Please enter a positive number of grades: ");
+            numGrades = input.nextInt();
+        }
+
+        int[] grades = new int[numGrades];
+
+        System.out.println("Enter " + numGrades + " grades seperated by spaces");
+
+        for (int i = 0, i <= numGrades, i++) {
+            grades[i] = input.nextInt();
+        }
+
+        System.out.println("You entered: ");
+        for (int i = 0, i < numGrades, i++) {
+            System.out.println(grades[i] + "");
+        }
+
+        // Once validated, confirm the number of grades
+        System.out.println("You will be processing " + numGrades + " grades.");
+
+        input.close();
     }
-
-    System.out.println("You will be processing " + numGrades + " grades. ");
-
-    
 }
